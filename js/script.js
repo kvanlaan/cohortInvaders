d85771e3504294fc6fa424f451f7f700cc525c7f
Katrina Van Laan <kdvanlaan@gmail.com>
2016-03-09 21:33:43 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 ;
 (function() {
     var Game = function(canvasId) {
+
+    	//query selectors
         var canvas = document.getElementById("screen");
         var screen = canvas.getContext('2d');
         var imgRect = document.getElementById("justin");
-         var imgInvader = document.getElementById("kaylan");
+        var imgJason = document.getElementById("jason");
+        var imgKaylan = document.getElementById("kaylan");
+        var imgJosh = document.getElementById("josh");
+        var imgThuy = document.getElementById("thuy");
+        var imgAdan = document.getElementById("adan");
+        var imgNatalia = document.getElementById("natalia");
+        var imgWensie = document.getElementById("wensie");
+        var imgBoaz = document.getElementById("boaz");
+        var imgMichael = document.getElementById("michael");
+        var imgAri = document.getElementById("ari");
+
+        //gameSize
         var gameSize = { x: canvas.width, y: canvas.height };
         console.log("hi")
-        this.bodies = createInvaders(this).concat(new Player(this, gameSize));
+            //this.bodies = createInvaders(this).concat(new Player(this, gameSize));
+
+        var createPlayer = function(game) {
+            var player = []
+            player.push(new Player(this, gameSize))
+            return player;
+        }
+
+
+        this.bodies = createPlayer(this, gameSize).concat(createInvaders(this));
+
         var self = this;
         loadSound("shoot.wav", function(shootSound) {
             self.shootSound = shootSound;
@@ -24,9 +47,9 @@
             var bodies = this.bodies;
             var notCollidingWithAnything = function(b1) {
                 return bodies.filter(function(b2) {
-                    return colliding(b1, b2); }).length === 0;
+                    return colliding(b1, b2);
+                }).length === 0;
             };
-
 
             this.bodies = this.bodies.filter(notCollidingWithAnything);
             for (var i = 0; i < this.bodies.length; i++) {
@@ -34,20 +57,25 @@
             }
         },
 
-        //drawInvade: function(screen, gameSize) {
-        	 //screen.clearRect(0, 0, gameSize.x, gameSize.y);
-            //for (var i = 0; i < this.bodies.length-1; i++) {
-                //drawInvaders(screen, this.bodies[i]);
-
-            //}
-        //},
-
         draw: function(screen, gameSize) {
             screen.clearRect(0, 0, gameSize.x, gameSize.y);
-            for (var i = 0; i < this.bodies.length; i++) {
-                drawRect(screen, this.bodies[i]);
+            drawJason(screen, this.bodies[1]);
+            drawKaylan(screen, this.bodies[2]);
+            drawAdan(screen, this.bodies[3]);
+            drawThuy(screen, this.bodies[4]);
+            drawNatalia(screen, this.bodies[5]);
+            drawWensie(screen, this.bodies[6]);
+            drawJosh(screen, this.bodies[7]);
+            drawKat(screen, this.bodies[8]);
+            drawThomas(screen, this.bodies[9]);
+            drawMichael(screen, this.bodies[10]);
+            drawBoaz(screen, this.bodies[11]);
 
+            drawRect(screen, this.bodies[0]);
+            for (var i = 12; i < this.bodies.length; i++) {
+                drawRect(screen, this.bodies[i]);
             }
+
         },
 
         addBody: function(body) {
@@ -62,7 +90,7 @@
             }).length > 0;
         }
     }
-
+//Invaders
     var Invader = function(game, center) {
         this.game = game;
         this.size = { x: 15, y: 15 };
@@ -96,7 +124,7 @@
         return invaders;
     }
 
-
+//bullets
     var Bullet = function(center, velocity) {
         this.size = { x: 3, y: 3 };
         this.center = center;
@@ -111,12 +139,14 @@
 
         }
     };
+//Player
     var Player = function(game, gameSize) {
         this.game = game;
-        this.size = { x: 15, y: 15 };
+        this.size = { x: 40, y: 40 };
         this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
         this.keyboarder = new Keyboarder();
     };
+    
 
     Player.prototype = {
         update: function() {
@@ -135,23 +165,92 @@
         }
     };
 
+   
+//drawingfolks
     var drawRect = function(screen, body) {
         //screen.fillRect(,
-           // 
-          //  body.size.x, body.size.y);//
-var imgRect = document.getElementById("justin");
-screen.drawImage(imgRect, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,body.size.x, body.size.y);
+        // 
+        //  body.size.x, body.size.y);//
+        var imgRect = document.getElementById("justin");
+        screen.drawImage(imgRect, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
     };
 
-    //var drawInvaders = function(screen, body) {
-        //
-           // 
-          //  body.size.x, body.size.y);//
-//var imgInvader = document.getElementById("kaylan");
-//screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,body.size.x, body.size.y);
-    //};//
+    var drawJason = function(screen, body) {
+
+        var imgInvader = document.getElementById("jason");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawKaylan = function(screen, body) {
+
+        var imgInvader = document.getElementById("kaylan");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawJosh = function(screen, body) {
+
+        var imgInvader = document.getElementById("josh");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawWensie = function(screen, body) {
+
+        var imgInvader = document.getElementById("wensie");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
 
 
+    var drawNatalia = function(screen, body) {
+
+        var imgInvader = document.getElementById("natalia");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawThuy = function(screen, body) {
+
+        var imgInvader = document.getElementById("thuy");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawAdan = function(screen, body) {
+
+        var imgInvader = document.getElementById("adan");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawKat = function(screen, body) {
+        var imgInvader = document.getElementById("katrina");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawMichael = function(screen, body) {
+
+        var imgInvader = document.getElementById("michael");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawBoaz = function(screen, body) {
+
+        var imgInvader = document.getElementById("boaz");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawAri = function(screen, body) {
+
+        var imgInvader = document.getElementById("ari");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+    var drawThomas = function(screen, body) {
+
+        var imgInvader = document.getElementById("thomas");
+        screen.drawImage(imgInvader, body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
+    };
+
+//enddrawingfolks
+
+
+//Keystrokes//
     var Keyboarder = function() {
         var keyState = {}
         window.onkeydown = function(e) {
